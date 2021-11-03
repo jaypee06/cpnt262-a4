aa361bf6da9b0d194a48c23b4fb2a9de3d50c62e
John paul Adobas <johnpaul.adobas@edu.sait.ca>
2021-11-03 16:26:54 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,38 @@
+
+
+const chalk = require('chalk')
+const processArray = process.argv.slice(2)
+const convertTo = processArray[0]
+const number = processArray[1]
+
+
+//Variable for the Output value.
+let answer = "";
+
+//Formula to Convert celcius to fahrenheit
+const toF = function (num1){
+    answer = (num1 * 9/5) + 32 
+    console.log(chalk.green(`${num1}°C to Celcius =  ${answer}°F` ))
+}
+//Formula to convert fahrenheit to celcius
+const toC = function(num2){
+    answer = (num2 - 32) * 5/9
+    console.log(chalk.green(`${num2}°F to Celcius =  ${answer}°C` ))
+}
+//Convertion to Fahrenheit
+
+if (convertTo === "toF"){
+
+    toF(number)
+}
+//Convertion to Celcius
+else if (convertTo === "toC"){
+
+    toC(number)
+}
+
+else{
+    console.log(chalk.red('Please follow instruction!!'))
+}
+
+
